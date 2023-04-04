9f13f726903035b4376cc607cda3a1233ff742ef
Rafael Pradita_PC <rafaeljonathan.work@gmail.com>
2023-04-04 11:15:49 +0700

    adding API login feature, change console.log to alert

--- src/asset/js/index.js
+++ src/asset/js/index.js
@@ -8,17 +8,30 @@ import {
 } from "../../lib/FormValidator.js";
 
 import { eventListenerSelect } from "../../lib/simplifyLib.js";
+import { USER_API_ENDPOINT } from "../../lib/constants.js";
 
-const getUserData = localStorage.getItem("userData");
-const userData = JSON.parse(getUserData) || [
-  { id: 1, email: "admin@gmail.com", password: "Adm123123" },
-];
-localStorage.setItem("userData", JSON.stringify(userData));
+// const getUserData = localStorage.getItem("userData");
+// const userData = JSON.parse(getUserData) || [
+//   { id: 1, email: "admin@gmail.com", password: "Adm123123" },
+// ];
+// localStorage.setItem("userData", JSON.stringify(userData));
 
-const signIn = () => {
+// getting all user
+const getAllUser = async () => {
+  try {
+    let response = await fetch(USER_API_ENDPOINT);
+    return await response.json();
+    
+  } catch (err) {
+    console.log("error", err);
+  }
+};
+
+const signIn = async () => {
   let email = document.querySelector("#emailLogin").value;
   let pw = document.querySelector("#pwLogin").value;
-  console.log(email, "email");
+  
+  const userData = await getAllUser();
 
   const checkCredential = userData.find(
     (a) => a.email === email && a.password === pw
@@ -26,63 +39,77 @@ const signIn = () => {
 
   switch (false) {
     case checkEmptyString(email):
-      console.log("Please fill in your email!");
+      alert("Please fill in your email!");
       break;
     case checkEmail(email):
-      console.log("Please enter a valid email!");
+      alert("Please enter a valid email!");
       break;
     case checkEmptyString(pw):
-      console.log("Please fill in your password!");
+      alert("Please fill in your password!");
       break;
     case checkCredential !== undefined:
-      console.log("You may enter wrong email or password.");
+      alert("You may enter wrong email or password.");
       break;
     case checkCredential === undefined:
-      console.log("success");
+      alert("success");
       localStorage.setItem("email", email);
       window.location.href = "./pages/dashboard/";
       break;
   }
 };
 
-const signUp = () => {
+const addUser = async (email, pwd) => {
+  try {
+    let response = await fetch(USER_API_ENDPOINT,{
+      method: 'POST',
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: pwd
+      })
+    });
+    return await response.json();
+  } catch (err) {
+    console.log('error',(err))
+  }
+}
+
+const signUp = async() => {
   let email = document.querySelector("#emailReg").value;
-  let pw = document.querySelector("#pwReg").value;
+  let pwd = document.querySelector("#pwReg").value;
+
+  const userData = await getAllUser();
 
   const checkDuplicate = userData.find((a) => a.email === email);
 
   switch (false) {
     case checkEmptyString(email):
-      console.log("Please fill in your email!");
+      alert("Please fill in your email!");
       break;
     case checkEmail(email):
-      console.log("Please enter a valid email!");
+      alert("Please enter a valid email!");
       break;
-    case checkEmptyString(pw):
-      console.log("Please fill in your password!");
+    case checkEmptyString(pwd):
+      alert("Please fill in your password!");
       break;
-    case checkStringLength(pw):
-      console.log("Your password must contain at least 8 characters!");
+    case checkStringLength(pwd):
+      alert("Your password must contain at least 8 characters!");
       break;
-    case checkUpperCase(pw):
-      console.log("Your password must contain at least 1 uppercase!");
+    case checkUpperCase(pwd):
+      alert("Your password must contain at least 1 uppercase!");
       break;
-    case checkLowerCase(pw):
-      console.log("Your password must contain at least 1 lowercase!");
+    case checkLowerCase(pwd):
+      alert("Your password must contain at least 1 lowercase!");
       break;
-    case checkNumber(pw):
-      console.log("Your password must contain at least 1 number!");
+    case checkNumber(pwd):
+      alert("Your password must contain at least 1 number!");
       break;
     case checkDuplicate === undefined:
-      console.log("This email has been registered");
+      alert("This email has been registered");
       break;
     case checkDuplicate !== undefined:
-      userData.push({
-        id: userData[userData.length - 1].id + 1,
-        email: email,
-        password: pw,
-      });
-      localStorage.setItem("userData", JSON.stringify(userData));
+      addUser(email,pwd);
+      alert('register Success')
       break;
   }
 };
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,1 +1,2 @@
 export const API_ENDPOINT = "https://6424eee79e0a30d92b257005.mockapi.io/Movie";
+export const USER_API_ENDPOINT = "https://6424eee79e0a30d92b257005.mockapi.io/User";
